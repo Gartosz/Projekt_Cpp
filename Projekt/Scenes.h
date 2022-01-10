684b02c3a50abe52a73f73aa203184a8d2a56140
Bartosz <45167802+Gartosz@users.noreply.github.com>
2022-01-10 23:59:10 +0100

    Delete Projekt directory

--- Projekt/Scenes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-
-int new_game(sf::RenderWindow& window, const sf::Font& font, int& start_value);
-void bunker(sf::RenderWindow& window, sf::Font const& font, int& start_value, const sf::Sprite& map);
